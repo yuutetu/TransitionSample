7e5692cfe8e99842aa49d8aecc61c4f4c3dfd387
加賀江 優幸 <yuutetu@kagakousachinoMacBook-Pro.local>
2017-11-19 23:06:50 +0900

    TabBarに適当にViewControllerを配置してみた

--- TransitionSample/Coordinators/RootTabBarCoodinator.swift
+++ TransitionSample/Coordinators/RootTabBarCoodinator.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class RootTabBarCoordinator: Coordinator {
     let tabBarController: UITabBarController
+    private(set) var aViewController: RootViewController = RootViewController.viewController(text: "A")
+    private(set) var bViewController: RootViewController = RootViewController.viewController(text: "B")
+    private(set) var cViewController: RootViewController = RootViewController.viewController(text: "C")
     
     init(tabBarController: UITabBarController) {
         self.tabBarController = tabBarController
+        aViewController.tabBarItem = UITabBarItem(title: "A", image: nil, selectedImage: nil)
+        bViewController.tabBarItem = UITabBarItem(title: "B", image: nil, selectedImage: nil)
+        cViewController.tabBarItem = UITabBarItem(title: "C", image: nil, selectedImage: nil)
     }
     
     func start() {
-        
+        tabBarController.viewControllers = [aViewController, bViewController, cViewController]
     }
 }
